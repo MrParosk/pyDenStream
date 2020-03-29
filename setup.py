c99e730404624e5b06960b97835c8c100633cb95
MrParosk <erik.parlstrand@gmail.com>
2020-03-29 17:21:06 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pyDenStream',
+   version='0.1',
+   description='TBC',
+   author='TBC',
+   author_email='TBC',
+   packages=['pyDenStream'],  #same as name
+   install_requires=['numpy', 'numba'], #external packages as dependencies
+)
